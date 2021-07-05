2bd065d1586323e9969c3a67db3c37eb42ff6b84
MRadzynski <radzynski.milosz@gmail.com>
2021-07-06 00:21:10 +0200

    Adding redux store, rootReducer and todoReducer

---
+++ src/redux/todo/todo.reducer.js
@@ -0,0 +1,12 @@
+const INTITIAL_STATE = {
+    todos: null,
+}
+
+const todoReducer = (state = INTITIAL_STATE, action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+};
+
+export default todoReducer;
