cd300e74528c021d0eee6e501aed929fa488a226
MRadzynski <radzynski.milosz@gmail.com>
2021-07-08 12:24:30 +0200

    Adding lazy loading for pages and spinner component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Homepage from './pages/Homepage/Homepage.jsx';
-import AddTodo from './pages/AddTodo/AddTodo.jsx';
-import EditTodo from './pages/EditTodo/EditTodo.jsx';
-import List from './pages/List/List.jsx';
-import PreviewTodo from './pages/PreviewTodo/PreviewTodo.jsx';
-import Settings from './pages/Settings/Settings.jsx';
-import NotFound from './components/NotFound/NotFound.jsx';
+import Spinner from './components/Spinner/Spinner';
+import NotFound from './components/NotFound/NotFound';
 import ErrorBourdary from './components/ErrorBoundary/ErrorBoundary';
+
+const Homepage = lazy(() => import('./pages/Homepage/Homepage'));
+
+const List = lazy(() => import('./pages/List/List'));
+
+const AddTodo = lazy(() => import('./pages/AddTodo/AddTodo'));
+
+const PreviewTodo = lazy(() => import('./pages/PreviewTodo/PreviewTodo'));
+
+const EditTodo = lazy(() => import('./pages/EditTodo/EditTodo'));
+
+const Settings = lazy(() => import('./pages/Settings/Settings'));
 
 const App = () => {
   return (
     <>
       <ErrorBourdary>
-        <Switch>
-          <Route exact path="/" component={Homepage} />
+        <Suspense fallback={<Spinner />}>
+          <Switch>
+            <Route exact path="/" component={Homepage} />
 
-          <Route exact path="/list" component={List} />
+            <Route exact path="/list" component={List} />
 
-          <Route exact path="/add-todo" component={AddTodo} />
+            <Route exact path="/add-todo" component={AddTodo} />
 
-          <Route exact path="/list/:id" component={PreviewTodo} />
+            <Route exact path="/list/:id" component={PreviewTodo} />
 
-          <Route path="/list/:id/edit" component={EditTodo} />
+            <Route path="/list/:id/edit" component={EditTodo} />
 
-          <Route exact path="/settings" component={Settings} />
+            <Route exact path="/settings" component={Settings} />
 
-          <Route component={NotFound} />
-        </Switch>
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
       </ErrorBourdary>
     </>
   );
